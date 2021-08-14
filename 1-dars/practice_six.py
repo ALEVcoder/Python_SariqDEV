d9c356b277e12a47adf8948ebd0db4d48db0deaa
ALEVcoder <71842904+ALEVcoder@users.noreply.github.com>
2021-08-14 19:48:40 +0300

    six lesson done

---
+++ 1-dars/practice_six.py
@@ -0,0 +1,26 @@
+# 6-Dars | Practice
+# Sana: 14.08.2021
+# Ko`chiruvchi: ALEVcoder
+
+
+
+# son = int(input('Istalgan soningizni kiriting \n==>'))
+# kvadrat = son ** 2
+# kub = son ** 3
+# print(f"""{son} ning kvadrati {kvadrat} ga teng \n
+# {son} ning kubi {kub} ga teng""")
+
+
+# +++++++++++++++++++++++++++++++++++++++++++
+# +++++++++++++++++++++++++++++++++++++++++++
+
+# yosh = int(input("Yoshingizni kiriting? \n ==>"))
+# yil = 2021 - yosh
+# print(f"Siz {yil} da tug`ulgansiz")
+
+# +++++++++++++++++++++++++++++++++++++++++++
+# +++++++++++++++++++++++++++++++++++++++++++
+
+son_one = int(input('Birinchi sonni yozing \n>>>'))
+son_two = int(input('Ikkinchi sonni yozing \n>>>'))
+print(f"\n{son_one} + {son_two} = {son_one+son_two}\n {son_one} - {son_two} = {son_one-son_two}\n {son_one} / {son_two} = {son_one/son_two}\n {son_one} * {son_two} = {son_one*son_two}\n")
---
+++ 1-dars/six.py
@@ -0,0 +1,72 @@
+# 6-Dars
+# Sana: 14.08.2021
+# Ko`chiruvchi: ALEVcoder
+
+
+# +++++++++++++++++++++++++++++++++++++++++++
+# +++++++++++++++++++++++++++++++++++++++++++
+
+# a = 20     #integer 
+# b = 20.5   # float
+# type(b)    #turini ko`rish
+
+# aholi_soni = 7_677_576_776
+# print(type(aholi_soni))
+# print("Aholi Soni: ", aholi_soni)
+
+# +++++++++++++++++++++++++++++++++++++++++++
+# +++++++++++++++++++++++++++++++++++++++++++
+
+# x, y, z = 20, 20.5, -56
+# # print(x*(y+z))
+
+# i = 100//2 #Integer
+# f = 100/2  #Float
+# print(type(i))
+# print(type(f))
+# print(i)
+# print(f)
+
+
+
+# +++++++++++++++++++++++++++++++++++++++++++
+# +++++++++++++++++++++++++++++++++++++++++++
+# MISOL
+
+# radius = 20
+# pi = 3.14159
+# diametr = 2*radius
+# print('Aylananing uzunligi: ', pi*diametr)
+
+
+
+# +++++++++++++++++++++++++++++++++++++++++++
+# +++++++++++++++++++++++++++++++++++++++++++
+# MISOL
+
+# #Error version
+# ism = "Samandar"
+# yosh = 16
+# xabar = ism + ' ' + yosh + ' yoshda'
+# print(xabar)
+
+# #True version
+# ism = "Samandar"
+# yosh = 16
+# xabar = ism + ' ' + str(yosh) + ' yoshda'
+# print(xabar)
+
+
+# +++++++++++++++++++++++++++++++++++++++++++
+# +++++++++++++++++++++++++++++++++++++++++++
+# MISOL
+
+# #Error version
+# t_yil = input('Tug`ulgan yilingzni kiriting ')
+# yosh = 2021 - t_yil
+# print("Siz ", yosh, " yoshda ekanszkuuuuuu")
+
+#True version
+t_yil = input('Tug`ulgan yilingzni kiriting ')
+yosh = 2021 - int(t_yil)
+print("Siz ", yosh, " yoshda ekanszkuuuuuu")
