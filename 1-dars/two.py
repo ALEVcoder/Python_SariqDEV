12340970afc78058d8680f1fbc796699ecac98f2
ALEVcoder <71842904+ALEVcoder@users.noreply.github.com>
2021-08-14 17:59:19 +0300

    two lesson

---
+++ 1-dars/two.py
@@ -0,0 +1,15 @@
+# 2-Dars
+# Sana: 14.08.2021
+# Ko`chiruvchi: ALEVcoder
+
+print('Assalomu alaykum')
+print('Hello world')
+
+# print(Hello world)
+print("Hayrli tong")
+print('Hayrli tong')
+print('Men "Univermak" markazidagi phone sotib oldim')
+print("""Samandar Assalomu alaykum
+Yaxshimisan charchash bo`mayabdimi? """)
+print("Samandar \n Assalomu alaykum")
+print("Samandar \n Assalomu alaykum, \n O\`quvchilarning barchasiga yordam ber")
