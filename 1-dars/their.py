addfc0f1d56189b6b371bfb8781e3a7aa10dd5bd
ALEVcoder <71842904+ALEVcoder@users.noreply.github.com>
2021-08-14 18:03:59 +0300

    their lesson done

---
+++ 1-dars/their.py
@@ -0,0 +1,11 @@
+# 2-Dars
+# Sana: 14.08.2021
+# Ko`chiruvchi: ALEVcoder
+
+print(2+4*2)
+print(19/5)
+print(20/5)
+print(16//4)
+print(2**4)
+print("To`qqizning kvadrati: ", 9**2, "ga teng")
+print('3 x 4 =',3*4)
