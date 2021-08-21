59ca5cc75f2dfd1d7cd00d4eee2cbebae929bdc3
ALEVcoder <71842904+ALEVcoder@users.noreply.github.com>
2021-08-21 17:38:53 +0300

    nine lesson done

---
+++ 1-dars/practice_nine.py
@@ -0,0 +1,17 @@
+# 9-Dars
+# Sana: 21.08.2021
+# Ko`chiruvchi: ALEVcoder
+
+
+# print('5 ta kino aytasiz hozir')
+# kinolar = []
+# for kino in range(5):
+#     kinolar.append(input(f'{kino+1}-kino nomi '))
+# print(kinolar)
+
+# print("Necha odam bilan suhbatlashdingiz ")
+# odam = int(input())
+# dostlar = []
+# for dost in range(odam):
+#     dostlar.append(input(f"{dost+1}-suhbatlashgan odamanigiz.. "))
+# print(dostlar)
