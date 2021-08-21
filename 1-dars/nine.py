dcc728e354ed5aba8ea67f422089c17904e29296
ALEVcoder <71842904+ALEVcoder@users.noreply.github.com>
2021-08-21 17:20:27 +0300

    nine lesson done

---
+++ 1-dars/nine.py
@@ -0,0 +1,34 @@
+# 9-Dars
+# Sana: 21.08.2021
+# Ko`chiruvchi: ALEVcoder
+
+# mehmonlar = ['Ali', 'Vali', 'Samandar', 'Husan', 'Umidjon']
+# for mehmon in mehmonlar:
+#     print('Assalomu alaykum', mehmon)
+#     print('Mayli, Yaxshi boring', mehmon)
+
+
+# mehmonlar = ['Ali', 'Vali', 'Samandar', 'Husan', 'Umidjon']
+# for taklif in mehmonlar:
+#     print(f""" Hurmatli {taklif}, \nSizni 30-dekabir kuni\nTo`yga taklif e`tamiz.
+#     \nHurmat bilan Palonchoyevlar oilasi.""")
+
+# sonlar = list(range(1,11))
+# for son in sonlar:
+#     print(f"{son} ning kvadrati {son**2} ga teng")
+# print('Barakalla')
+
+
+# sonlar = list(range(11))
+# sonlar_kv = []
+# for son in sonlar:
+#     sonlar_kv.append(son**2)
+# print(sonlar)
+# print(sonlar_kv)
+
+
+dostlar = []
+print('5 ta eng yaqin do`stingiz kim?')
+for n in range(5):
+    dostlar.append(input(f"{n+1}-do`stingizni kiriting..."))
+print(dostlar)
