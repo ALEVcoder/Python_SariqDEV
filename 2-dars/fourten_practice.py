e2dfb4a1bd18e38240cbec44c2bc5d8dc9c81347
ALEVcoder <71842904+ALEVcoder@users.noreply.github.com>
2021-08-22 18:14:48 +0300

    fourten practice done

---
+++ 2-dars/fourten_practice.py
@@ -0,0 +1,39 @@
+# 14-Dars | Practice
+# Sana: 22.08.2021
+# Ko`chiruvchi: ALEVcoder
+
+
+otam = {'ismi':'mavlutdin', 'tyil':1954,'viloyat':'samarqand'}
+tyil = otam['tyil']
+vil = otam['viloyat']
+print(f"Otamning ismi {otam['ismi'].title()}, {tyil}-yilda, {vil.title()} viloyatida tug'ilgan")
+
+taomlar = {
+    'ali':'osh',
+    'vali':'shashlik',
+    'hasan':"lag'mon",
+    'husan':"mastava",
+    'olim':"somsa"
+    }
+
+taom = taomlar['ali']
+print(f"Alining sevimli taomi {taom}")
+
+python_izohli_lugati = {
+    'integer':"Butun son",
+    'float':"O'nlik son",
+    'string':"Matn",
+    'list':"Ro'yxat",
+    'tuple':"O'zgarmas ro'yxat"}
+# print(python_izohli_lugati['tuple'])
+
+kalit = input("Kalit so'z kiriting:").lower()
+print(python_izohli_lugati.get(kalit,"Bunday so'z mavjud emas"))
+
+kalit = input("Kalit so'z kiriting:").lower()
+tarjima = python_izohli_lugati.get(kalit)
+if tarjima==None:
+    print("Bunday so'z mavjud emas")
+else:
+    print(f"{kalit.title()} so'zi {tarjima} deb tarjima qilinadi")
+    
