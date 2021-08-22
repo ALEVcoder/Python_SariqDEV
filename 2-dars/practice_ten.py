fa8ac5c7128b5ed1244559835bc2387ff55f40af
ALEVcoder <71842904+ALEVcoder@users.noreply.github.com>
2021-08-22 16:01:52 +0300

    ten lesson done

---
+++ 2-dars/practice_ten.py
@@ -0,0 +1,4 @@
+# 10-Dars | Practice
+# Sana: 22.08.2021
+# Ko`chiruvchi: ALEVcoder
+
---
+++ 2-dars/ten.py
@@ -0,0 +1,77 @@
+# 10-Dars
+# Sana: 22.08.2021
+# Ko`chiruvchi: ALEVcoder
+
+# avtolar = ['damas', 'nexia', 'juguli', 'moskvich', 'cobolt']
+
+# for avto in avtolar:
+#     if avto == 'nexia':
+#         print(avto.upper())
+#     else:
+#         print(avto.title())
+
+
+# ==========================================================
+# ==========================================================
+# MISOL
+
+# ism = input('Ismingiz nima?')
+# if ism.lower() == 'ali':
+#     print('Salom Ali')
+# else:
+#     print(f'Uzr, {ism} biz Alini kutyabmiz')
+
+# ==========================================================
+# ==========================================================
+# MISOL
+
+# javob = float(input('12x6 nechiga teng'))
+# if javob != 72:
+#     print('Javob xato')
+
+# ==========================================================
+# ==========================================================
+# MISOL
+
+# yosh = int(input('Yoshingiz nechida'))
+# if yosh >= 18:
+#     print('Xush kelibsiz!')
+# else:
+#     print('Kirish mumkin emas')
+
+# ==========================================================
+# ==========================================================
+# MISOL
+
+# login = input('Loginni kiriting')
+# if len(login)<=8:
+#     print('Login 8 ta harfdan iborat bo`lishi shart')
+# print('Dastur tugadi')
+
+
+# ==========================================================
+# ==========================================================
+# MISOL
+
+# yil = int(input('Tug`ulgan yilingizni kiriting '))
+# if 2021-yil < 18:
+#     print(f'Yoshingiz {2021-yil} da ekan')
+#     print('Kirish mumkin emas')
+# else:
+#     print('Xush kelibsiz')
+
+
+# ==========================================================
+# ==========================================================
+# MISOL
+
+# yosh = int(input('Yoshingz nechida'))
+# if yosh>=65:print('Siz Covid-203 gruhda ekansiz')
+
+
+# ==========================================================
+# ==========================================================
+# MISOL
+
+x,y = 20, 50
+print('x>y') if x>y else print('x<y')
