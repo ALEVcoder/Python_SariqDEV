4e777a54d8e5330acb6ba583da191d7382e985ad
ALEVcoder <71842904+ALEVcoder@users.noreply.github.com>
2021-08-22 16:30:02 +0300

    ten practice done

--- 2-dars/practice_ten.py
+++ 2-dars/practice_ten.py
@@ -2,3 +2,63 @@
 # Sana: 22.08.2021
 # Ko`chiruvchi: ALEVcoder
 
+
+# ========================================
+# ========================================
+# MISOL
+
+# cars = ['damas', 'gm', 'nexia', 'juguli', 'moskvich', 'cobolt']
+
+# for car in cars:
+#     if car != 'gm':
+#         print(car.title())
+#     else:
+#         print(car.upper())
+
+
+# ========================================
+# ========================================
+# MISOL
+
+
+# login = input('Logini kiiriting ')
+
+# if login.lower() != 'admin':
+#     print(f'Xush kelibsiz {login}')
+# else:
+#     print(f'Xush kelibsiz {login}. Userlar ruyxatini ko`rasizmi?')
+
+
+# ========================================
+# ========================================
+# MISOL
+
+# son_one = int(input('Son bir '))
+# son_two = int(input('Son ikki '))
+
+# if son_one != son_two:
+#     print('Sonlar bir biriga teng emas')
+# else:
+#     print('Sonlar teng')
+
+# ========================================
+# ========================================
+# MISOL
+
+# son = int(input('Son kiriting '))
+
+# if son > 0:
+#     print(son, 'Musbat son')
+# else:
+#     print(son, 'Manfiy son')
+
+# ========================================
+# ========================================
+# MISOL
+
+son = int(input('Son kiriting '))
+
+if son > 0:
+    print(son, 'sonining ildizi ', son**2)
+else:
+    print(son, 'Musbat son kiriting.')
